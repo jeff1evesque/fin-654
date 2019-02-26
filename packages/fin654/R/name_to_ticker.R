a9f54d1d5a502b5e111537121be843407d725df8
jeffrey levesque <jeff1evesque@yahoo.com>
2019-02-25 22:21:04 -0500

    #7: name_to_ticker.R, implement 'hash' package

--- packages/fin654/R/name_to_ticker.R
+++ packages/fin654/R/name_to_ticker.R
@@ -1,7 +1,16 @@
 ##
 ## name_to_ticker.R, convert company name to ticker.
 ##
-name_to_ticker = function(name) {
+## @s, series to match and replace
+## @fps, list of file references.
+##
+name_to_ticker = function(s, fps) {
+  ## load data
+  context = hash()
+  for (i in 1:length(fps)) {
+    context[i] = Dataframe(fps[i])
+  }
+
   ## return company name
   return()
 }
