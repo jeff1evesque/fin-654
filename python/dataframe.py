e6f351930594236ed0a85051ff7d1aacdfeb9dbf
jeffrey levesque <jeff1evesque@yahoo.com>
2019-03-02 20:51:16 -0500

    #7: dataframe.py, ensure list of unacceptable strings

--- python/dataframe.py
+++ python/dataframe.py
@@ -59,19 +59,22 @@ class Dataframe:
 
         self.df.dropna(inplace=True)
 
-    def remove_rows(self, cols, str):
+    def remove_rows(self, cols, items):
         '''
 
         remove any rows where specified column contains a specified string.
 
         @cols, list of columns to check for unacceptable strings.
-        @str, list of unacceptable strings
+        @items, list of unacceptable strings
 
         '''
 
+        if isinstance(items, str):
+            items = [items]
+
         for c in cols:
             if c in self.df:
-                self.df = self.df[~self.df[c].isin(str)]
+                self.df = self.df[~self.df[c].isin(items)]
 
     def remove_cols(self, cols):
         '''
