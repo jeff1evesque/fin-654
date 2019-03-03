6e5baa0c5b89fb07801bfa10453fa6dbc03038d0
jeffrey levesque <jeff1evesque@yahoo.com>
2019-03-03 14:08:33 -0500

    #7: add 'finalize_dataset' function

---
+++ packages/fin654/R/finalize_dataset.R
@@ -0,0 +1,16 @@
+##
+## final_dataset.R, finalizes dataset by reducing based on common 'tickers',
+##     for a specified 'column'
+##
+finalize_dataset = function(df, column, tickers, spath) {
+  ## source python
+  for (p in spath) {
+    source_python(p)
+  }
+
+  ## load data
+  df.final = Dataframe(df, 'False')
+
+  ## dataframe with tickers column
+  return(df.final$subset_on_col(column, tickers))
+}
