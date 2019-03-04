e17925144ee8b19452b87738a87a386e85570c1b
jeffrey levesque <jeff1evesque@yahoo.com>
2019-03-03 19:10:34 -0500

    #7: finalize_dataset.R< refactor method arguments

--- packages/fin654/R/finalize_dataset.R
+++ packages/fin654/R/finalize_dataset.R
@@ -1,8 +1,8 @@
 ##
-## final_dataset.R, finalizes dataset by reducing based on common 'tickers',
-##     for a specified 'column'
+## final_dataset.R, finalizes dataset by reducing based on common company,
+##     name (i.e. ref$name) for a specified 'column'
 ##
-finalize_dataset = function(df, column, tickers, spath) {
+finalize_dataset = function(df, column, ref, spath) {
   ## source python
   for (p in spath) {
     source_python(p)
@@ -12,6 +12,7 @@ finalize_dataset = function(df, column, tickers, spath) {
   df.final = Dataframe(df, 'False')
 
   ## dataframe with tickers column
-  df.final$subset_on_col(column, tickers)
+  df.final$subset_on_col(column, ref$name)
+  df.final$set_column(column, ref, 'symbol')
   return(df.final$get_df())
 }
