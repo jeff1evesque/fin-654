330119891f8d5fc064c2349bbbbd0826e0b318fa
jeff1evesque <jeff1evesque@yahoo.com>
2019-03-25 14:52:36 +0000

    #24: app.R, restructure gpd using 'reactive'

--- app.R
+++ app.R
@@ -169,6 +169,60 @@ server = function(input, output, session) {
     })
   })
 
+  data.gpd = reactive({
+    data = na.omit(df.ts)
+    data.cbind = cbind(
+      df.ts$blw$open,
+      df.ts$gpn$open,
+      df.ts$ms$open,
+      df.ts$dal$open,
+      df.ts$sti$open,
+      df.ts$fb$open,
+      df.ts$mar$open
+    )
+    data.r = diff(log(as.matrix(data.cbind))) * 100
+    price.last = as.numeric(tail(data.r, n=1))
+    position.rf = c(1/3, 1/3, 1/3)
+    w = position.rf * price.last
+    weights.rf = matrix(w, nrow=nrow(data.r), ncol=ncol(data.r), byrow=TRUE)
+    loss.rf = -rowSums(expm1(data.r/100) * weights.rf)
+    
+    d    =  as.vector(loss.rf)          # data is purely numeric
+    umin =  min(d)                      # threshold u min
+    umax =  max(d) - 0.1                # threshold u max
+    nint = 100                          # grid length to generate mean excess plot
+    u = seq(umin, umax, length = nint)  # threshold u grid
+    loss.excess = loss.rf[loss.rf > u]
+    
+    alpha.tolerance = 0.95
+    u = quantile(loss.rf, alpha.tolerance , names=FALSE)
+    fit = fit.GPD(loss.rf, threshold=u) # Fit GPD to the excesses
+    xi.hat = fit$par.ests[['xi']]       # fitted xi
+    beta.hat = fit$par.ests[['beta']]   # fitted beta
+    
+    n.relative.excess = length(loss.excess) / length(loss.rf) # = N_u/n
+    VaR.gpd = u + (beta.hat/xi.hat) * (((1-alpha.tolerance) / n.relative.excess)^(-1*xi.hat)-1)
+    ES.gpd = (VaR.gpd + beta.hat - xi.hat*u) / (1-xi.hat)
+    
+    loss.rf = -rowSums(expm1(data.r/100) * weights.rf)
+    loss.rf.df = data.frame(
+      Loss = loss.rf,
+      Distribution = rep(
+        'Historical',
+        each = length(loss.rf)
+      )
+    )
+
+    ## bundle results into hash
+    return(
+      hash(
+        'VaR.gpd' = VaR.gpd,
+        'ES.gpd' = ES.gpd,
+        'loss.rf.df' = loss.rf.df
+      )
+    )
+  })
+
   ##
   ## plot timeseries: lapply and 'local({})' workaround ensures
   ##     unique assignment. Otherwise, last plot used.
@@ -226,59 +280,21 @@ server = function(input, output, session) {
   ## gpd
   ##
   output$gpd = renderPlotly({
-    data = na.omit(df.ts)
-    data.cbind = cbind(
-      df.ts$blw$open,
-      df.ts$gpn$open,
-      df.ts$ms$open,
-      df.ts$dal$open,
-      df.ts$sti$open,
-      df.ts$fb$open,
-      df.ts$mar$open
-    )
-    data.r = diff(log(as.matrix(data.cbind))) * 100
-    price.last = as.numeric(tail(data.r, n=1))
-    position.rf = c(1/3, 1/3, 1/3)
-    w = position.rf * price.last
-    weights.rf = matrix(w, nrow=nrow(data.r), ncol=ncol(data.r), byrow=TRUE)
-    loss.rf = -rowSums(expm1(data.r/100) * weights.rf)
+    r.gpd = data.gpd()
+    VaR.gpd = r.gpd$VaR.gpd
+    ES.gpd = r.gpd$ES.gpd
+    loss.rf.df = r.gpd$loss.rf.df
 
-    d    =  as.vector(loss.rf)          # data is purely numeric
-    umin =  min(d)                      # threshold u min
-    umax =  max(d) - 0.1                # threshold u max
-    nint = 100                          # grid length to generate mean excess plot
-    u = seq(umin, umax, length = nint)  # threshold u grid
-    loss.excess = loss.rf[loss.rf > u]
-
-    alpha.tolerance = 0.95
-    u = quantile(loss.rf, alpha.tolerance , names=FALSE)
-    fit = fit.GPD(loss.rf, threshold=u) # Fit GPD to the excesses
-    xi.hat = fit$par.ests[["xi"]]       # fitted xi
-    beta.hat = fit$par.ests[["beta"]]   # fitted beta
-
-    n.relative.excess = length(loss.excess) / length(loss.rf) # = N_u/n
-    VaR.gpd = u + (beta.hat/xi.hat)*(((1-alpha.tolerance) / n.relative.excess)^(-1*xi.hat)-1)
-    ES.gpd = (VaR.gpd + beta.hat-xi.hat*u) / (1-xi.hat)
-
-    loss.rf = -rowSums(expm1(data.r/100) * weights.rf)
-    loss.rf.df = data.frame(
-      Loss = loss.rf,
-      Distribution = rep("Historical",
-                         each = length(loss.rf))
-    )
-
-    VaRgpd.text = paste("GPD: Value at Risk =", round(VaR.gpd, 2))
-    ESgpd.text = paste("Expected Shortfall =", round(ES.gpd, 2))
-    title.text = paste(VaRgpd.text, ESgpd.text, sep = " ")
+    VaRgpd.text = paste('GPD: Value at Risk =', round(VaR.gpd, 2))
+    ESgpd.text = paste('Expected Shortfall =', round(ES.gpd, 2))
+    title.text = paste(VaRgpd.text, ESgpd.text, sep = ' ')
     loss.plot = ggplot(loss.rf.df, aes(x = Loss, fill = Distribution)) +
       geom_density(alpha = 0.2)
     loss.plot = loss.plot +
-      geom_vline(aes(xintercept = VaR.gpd), colour = "blue", linetype = "dashed", size = 0.8)
-    loss.plot = loss.plot + geom_vline(aes(xintercept = ES.gpd), colour = "blue", size = 0.8) 
-    #+ annotate("text", x = 300, y = 0.0075, label = VaRgpd.text, colour = "blue") + annotate("text", x = 300, y = 0.005, label = ESgpd.text, colour = "blue")
-    loss.plot = loss.plot + xlim(0,500) +
-      ggtitle(title.text)
-    ggplotly(loss.plot)
+      geom_vline(aes(xintercept = VaR.gpd), colour = 'blue', linetype = 'dashed', size = 0.8)
+    loss.plot = loss.plot + geom_vline(aes(xintercept = ES.gpd), colour = 'blue', size = 0.8)
+    loss.plot = loss.plot + xlim(0,500) + ggtitle(title.text)
+    print(ggplotly(loss.plot))
   })
 }
 
