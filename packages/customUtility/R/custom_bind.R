31b8d377e89b386d74ed3fd9509c2ac118ec4aa6
jeff1evesque <jeff1evesque@yahoo.com>
2019-03-26 17:34:28 +0000

    #24: custom_bind.R, refactor 'cbind' implementation

--- packages/customUtility/R/custom_bind.R
+++ packages/customUtility/R/custom_bind.R
@@ -1,30 +1,15 @@
 ##
-## custom_bind.R, binds provided input list as either matrix or dataframe.
+## custom_bind.R, binds provided input list as matrix.
 ##
-custom_bind = function(data, is_matrix) {
-  ## local variables
-  dlength = length(data)
-  data = na.omit(data)
-  if (missing(is_matrix)) {
-    return_type = 'dataframe'
-  }
-
-  ## max length
+custom_bind = function(data) {
   max_length = Inf
-  for (i in 1:dlength) {
-    max_length = min(max_length, data[[i]])
+  for (d in data) {
+    max_length = min(max_length, length(d))
   }
 
-  ## bind items
   result=cbind()
-  if (return_type == 'dataframe') {
-    for (i in 1:dlength) {
-      result=cbind(result, data.frame(data[[i]][1:max_length]))
-    }
-  } else {
-    for (i in 1:dlength) {
-      result=cbind(result, matrix(data[[i]][1:max_length]))
-    }
+  for (d in data) {
+    result=cbind(result, matrix(d[1:max_length]))
   }
 
   return(result)
