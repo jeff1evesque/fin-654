67e6f66426cb898b77b617516fcd2969d193d5ad
jeff1evesque <jeff1evesque@yahoo.com>
2019-03-26 18:55:56 +0000

    #24: gpd.R, remove excess spacing

--- packages/fin654/R/gpd.R
+++ packages/fin654/R/gpd.R
@@ -14,24 +14,24 @@ gpd_compute = function(data) {
   w = position.rf * price.last
   weights.rf = matrix(w, nrow=nrow(data.r), ncol=ncol(data.r), byrow=TRUE)
   loss.rf = -rowSums(expm1(data.r/100) * weights.rf)
-    
+  
   d    =  as.vector(loss.rf)                                # data is purely numeric
   umin =  min(d)                                            # threshold u min
   umax =  max(d) - 0.1                                      # threshold u max
   nint = 100                                                # grid length to generate mean excess plot
   u = seq(umin, umax, length = nint)                        # threshold u grid
   loss.excess = loss.rf[loss.rf > u]
-    
+  
   alpha.tolerance = 0.95
   u = quantile(loss.rf, alpha.tolerance , names=FALSE)
   fit = fit.GPD(loss.rf, threshold=u)                       # Fit GPD to the excesses
   xi.hat = fit$par.ests[['xi']]                             # fitted xi
   beta.hat = fit$par.ests[['beta']]                         # fitted beta
-    
+  
   n.relative.excess = length(loss.excess) / length(loss.rf) # = N_u/n
   VaR.gpd = u + (beta.hat/xi.hat) * (((1-alpha.tolerance) / n.relative.excess)^(-1*xi.hat)-1)
   ES.gpd = (VaR.gpd + beta.hat - xi.hat*u) / (1-xi.hat)
-    
+
   loss.rf = -rowSums(expm1(data.r/100) * weights.rf)
   loss.rf.df = data.frame(
     Loss = loss.rf,
