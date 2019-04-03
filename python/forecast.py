45ba242c8088e52b73d26345d148bdf88dbc8944
jeff1evesque <jeff1evesque@yahoo.com>
2019-04-03 16:03:45 +0000

    #14: forecast.py, refactor 'Lstm' class

--- python/forecast.py
+++ python/forecast.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
-import pandas as pd
-import numpy as np
 from keras.models import Sequential
-from keras.layers import LSTM,Dense
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import load_model
 from sklearn.model_selection import train_test_split
 
 
@@ -16,7 +15,7 @@ class Lstm():
 
     '''
 
-    def __init__(self, data):
+    def __init__(self, data, train=False):
         '''
 
         define class variables.
@@ -24,43 +23,17 @@ class Lstm():
         '''
 
         self.data = data
-        self.look_back = 40
-        self.forward_days = 10
-        self.num_periods = 20
-        self.num_companies = len(self.data)
-
         for df in self.data:
             df.set_index('date', inplace=True)
 
         # execute model
-        self.normalize()
         self.split_data()
+        self.normalize()
 
-    def normalize(self):
-        '''
-
-        normalize current data.
-
-        '''
-
-        scl = MinMaxScaler()
-        args = [df.values.reshape(df.shape[0],1) for df in self.data]
-        self.array = scl.fit_transform(
-            np.concatenate(set(arg for arg in args), axis=1)
-        )
-
-    def utility_split(self, data, jump=1):
-        '''
-
-        utility function used by 'split_data'.
-
-        '''
-
-        X,Y = [],[]
-        for i in range(0, len(data) - self.look_back - self.forward_days + 1, jump):
-            X.append(data[i:(i+self.look_back)])
-            Y.append(data[(i+self.look_back):(i+self.look_back+self.forward_days)])
-        return(np.array(X), np.array(Y))
+        # train
+        if train:
+            self.train()
+            self.predict_test()
 
     def split_data(self, test_size=0.20):
         '''
@@ -71,49 +44,98 @@ class Lstm():
 
         '''
 
-        division = len(self.array) - self.num_periods * self.forward_days
-        array_test = self.array[division-self.look_back:]
-        array_train = self.array[:division]
-        X_test,y_test = utility_split(array_test)
-        y_test = np.array([list(a.ravel()) for a in y_test])
-        X,y = utility_split(array_train)
-        y = np.array([list(x.ravel()) for x in y])
-        self.X_train, self.X_validate, self.y_train, self.y_validate = train_test_split(
-            X,
-            y,
-            test_size=test_size,
-            random_state=42
-        )
+        self.train, self.test = train_test_split(self.data, test_size=test_size)
+        train_set = self.train['open']
+        test_set = self.test['open']
 
-    def train(self, num_first_layer=200, num_second_layer=100, epochs=50):
+        self.df_train = pd.DataFrame(train_set)
+        self.df_test = pd.DataFrame(test_set)
+
+    def normalize(self):
+        # scaling normalization
+        self.sc = MinMaxScaler(feature_range = (0, 1))
+        training_set_scaled = self.sc.fit_transform(self.df_train)
+
+        # Creating a data structure with 60 timesteps and 1 output
+        X_train = []
+        y_train = []
+        for i in range(60, 1258):
+            X_train.append(training_set_scaled[i-60:i, 0])
+            y_train.append(training_set_scaled[i, 0])
+        X_train, self.y_train = np.array(X_train), np.array(y_train)
+
+        # Reshaping
+        self.X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+    def train(self, epochs=50):
         '''
 
         train lstm model.
 
         '''
 
-        model = Sequential()
-        model.add(LSTM(
-            num_first_layer,
-            input_shape=(self.look_back, self.num_companies),
-            return_sequences=True
-        ))
-        model.add(LSTM(
-            num_second_layer,
-            input_shape=(num_first_layer, 1)
-        ))
-        model.add(Dense(forward_days * self.num_companies))
-        model.compile(loss='mean_squared_error', optimizer='adam')
+        # Initialize RNN
+        self.regressor = Sequential()
 
-        self.train = model.fit(
+        # Adding the first LSTM layer and some Dropout regularisation
+        self.regressor.add(LSTM(
+            units = 50,
+            return_sequences = True,
+            input_shape = (self.X_train.shape[1], 1)
+        ))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding a second LSTM layer and some Dropout regularisation
+        self.regressor.add(LSTM(
+            units = 50,
+            return_sequences = True
+        ))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding a third LSTM layer and some Dropout regularisation
+        self.regressor.add(LSTM(
+            units = 50,
+            return_sequences = True
+        ))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding a fourth LSTM layer and some Dropout regularisation
+        self.regressor.add(LSTM(units = 50))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding the output layer
+        self.regressor.add(Dense(units = 1))
+
+        # Compiling the RNN
+        self.regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+        # Fitting the RNN to the Training set
+        self.regressor.fit(
             self.X_train,
             self.y_train,
-            epochs=epochs,
-            validation_data=(self.X_validate, self.y_validate),
-            shuffle=True,
-            batch_size=1,
-            verbose=2
+            epochs = epochs,
+            batch_size = 32
         )
+
+    def predict_test(self):
+        dataset_total = pd.concat(
+            (self.train['open'], self.test['open']),
+            axis = 0
+        )
+        inputs = dataset_total[len(dataset_total) - len(self.test) - 60:].values
+        inputs = inputs.reshape(-1,1)
+        inputs = self.sc.transform(inputs)
+        X_test = []
+
+        for i in range(60, 80):
+            X_test.append(inputs[i-60:i, 0])
+
+        X_test = np.array(X_test)
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+        predicted_stock_price = regressor.predict(X_test)
+        predicted_stock_price = self.sc.inverse_transform(predicted_stock_price)
+
+        return(pd.DataFrame(predicted_stock_price))
 
     def get_model(self):
         '''
@@ -122,4 +144,5 @@ class Lstm():
 
         '''
 
-        return(self.train)
+        return(self.regressor)
+
