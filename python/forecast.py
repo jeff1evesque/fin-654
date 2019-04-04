30eeee2f857290f3cf946755a619ba2268175f34
jeff1evesque <jeff1evesque@yahoo.com>
2019-04-04 11:01:10 +0000

    #14: forecast.py, correctly reference 'self.regressor'

--- python/forecast.py
+++ python/forecast.py
@@ -153,7 +153,7 @@ class Lstm():
 
         X_test = np.array(X_test)
         X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-        predicted_stock_price = regressor.predict(X_test)
+        predicted_stock_price = self.regressor.predict(X_test)
         predicted_stock_price = self.sc.inverse_transform(predicted_stock_price)
 
         return(pd.DataFrame(predicted_stock_price))
