ad973cfcbe6d825ef7d4ae22b367d9ae987952e9
jeffrey levesque <jeff1evesque@yahoo.com>
2019-04-06 22:50:42 -0400

    #14: rnn_forecast.R, add 'plot_lstm'

---
+++ packages/fin654/R/rnn_forecast.R
@@ -0,0 +1,32 @@
+##
+## rnn_forecast.R, generate lstm model plot.
+##
+
+plot_lstm = function(model, index) {
+  ##
+  ## @index, 1 indicates train, 2 indicates test.
+  ##
+  if (index ==1) {
+    title = 'Train'
+  } else {
+    title = 'Test'
+  }
+
+  dates = model$get_index()
+  actual = model$get_actual()[[index]]
+  predicted_ = model$predict_test()[[index]]
+  
+  ## dataframes for multi-timeseries plot
+  df = data.frame(
+    date=head(c(dates), length(predicted)),
+    predicted=predicted,
+    actual=t(actual)
+  )
+  
+  ## generate plots
+  gg.train = ggplot(df, aes(x=date)) +
+    geom_line(aes(y=predicted, group=1), color='#00AFBB') +
+    geom_line(aes(y=actual, group=1), color='#FC4E07') +
+    ggtitle(paste0(title, 'ing Data') +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+}
