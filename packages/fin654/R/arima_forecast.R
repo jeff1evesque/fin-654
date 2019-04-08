7271f29310c2ec9a074eb864cceadc5e3f6e737d
jeff1evesque <jeff1evesque@yahoo.com>
2019-04-08 17:55:40 +0000

    #51: create + debug initial 'plot_arima'

---
+++ packages/fin654/R/arima_forecast.R
@@ -0,0 +1,29 @@
+##
+## arima_forecast.R, generate arima model plot.
+##
+
+plot_arima = function(model, index) {
+  ## local variables
+  dates = model$get_index()
+  scores = model$get_actual()
+
+  print(paste0('scores: ', scores))
+#  predicted = scores
+#  actual = scores
+
+  ## dataframes for multi-timeseries plot
+  df = data.frame(
+    date=head(c(dates), length(predicted)),
+    predicted=predicted,
+    actual=t(actual)
+  )
+  
+  ## generate plots
+  g = ggplot(df, aes(x=date)) +
+    geom_line(aes(y=predicted, group=1), color='#00AFBB') +
+    geom_line(aes(y=actual, group=1), color='#FC4E07') +
+    ggtitle('Arima forecast') +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+  return(g)
+}
