b9512a0859eefcde1e9a155a851956a33baecfbb
jeff1evesque <jeff1evesque@yahoo.com>
2019-04-10 19:08:01 +0000

    #51: arima_forecast.R, use 'predicted' tail length

--- packages/fin654/R/arima_forecast.R
+++ packages/fin654/R/arima_forecast.R
@@ -30,7 +30,7 @@ plot_arima = function(model, index) {
 
     ## dataframes for multi-timeseries plot
     df = data.frame(
-      date=tail(c(dates), length(actual)),
+      date=tail(c(dates), length(predicted)),
       predicted=predicted,
       actual=actual
     )
