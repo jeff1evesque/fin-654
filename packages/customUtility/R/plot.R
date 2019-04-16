034ebd0deee9dfc7b6fc652eda801cce6b8c29d2
jeff1evesque <jeff1evesque@yahoo.com>
2019-04-16 14:28:04 +0000

    #56: add 'plot_bar_graph' function

---
+++ packages/customUtility/R/plot.R
@@ -0,0 +1,34 @@
+##
+## plot.R, generalized ggplot constructions
+##
+## @data, needs to be a single column dataframe:
+##
+##        sums
+##     a  122
+##     b   23
+##     c  321
+##
+## Note: this function requires 'ggplot' package.
+##
+plot_bar_graph = function(df, y_label, x_label, title) {
+  if (missing(title)) {
+    title = paste0(y_label, ' vs. ', x_label)
+  }
+
+  return(
+    ggplot(
+      data = df,
+      aes(x=rownames(df), y=value, fill=factor(rownames(df)))) + 
+      geom_bar(position = 'dodge', stat = 'identity') +
+      ylab(y_label) +
+      xlab(x_label) +
+      theme(
+        legend.position='bottom',
+        plot.title = element_text(size=15, face='bold')
+      ) + 
+      ggtitle(title) +
+      labs(fill=rownames(melt.col_sum)) +
+      guides(fill=guide_legend(title=x_label)
+    )
+  )
+}
